4058898a0fcd1a0cf920dcd052803ad0803f3f5c
Therese Grass <tg222kv@student.lnu.se>
2022-02-24 09:22:44 +0100

    adding updateState method

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -16,20 +16,46 @@ export class IssuesController {
     try {
       console.log('index issues-controller')
       const fetchedData = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues?private_token=${process.env.TOKEN}`)
-      const data2 = await fetchedData.json()
-      const data = data2.map(data => ({
+      const result = await fetchedData.json()
+      const data = result.map(data => ({
 
         id: `${data.id}`,
+        iid: `${data.iid}`,
         title: `${data.title}`,
+        description: `${data.description}`,
         author: `${data.author.name}`,
         state: `${data.state}`
       }))
 
-     /*  console.log(result)
-      console.log(data) */
       res.render('../views/issues/issues', { data })
     } catch (error) {
       next(error)
     }
   }
+
+  /**
+   * Updates the state of a specific issue.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async updateState (req, res, next) {
+    const id = Object.keys(req.body)[0]
+    const state = Object.values(req.body)[0]
+    console.log(id, state)
+    let currentState = ''
+
+    currentState = state === 'opened' ? 'close' : 'reopen'
+    const respons = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues/${id}?state_event=${currentState}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer + ${process.env.TOKEN}`
+      }
+    })
+    console.log(respons)
+  }
 }
+
+// "https://gitlab.example.com/api/v4/projects/4/issues/85?state_event=close"
+      /*  'Content-type': 'application/json', */ // how to tell if it's needed? https://dmitripavlutin.com/fetch-with-json/
--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -5,3 +5,4 @@ export const router = express.Router()
 const controller = new IssuesController()
 
 router.get('/', (req, res, next) => controller.index(req, res, next))
+router.post('/', (req, res, next) => controller.updateState(req, res, next))
